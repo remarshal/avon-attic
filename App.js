ff3381741007de203503b9bf240d4f1162adbee3
remarshal <marshalwaldendev@gmail.com>
2022-05-27 14:22:39 -0400

    setup welcome screen

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
-import { StyleSheet, SafeAreaView, Button } from "react-native";
+import React from "react";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Button title="Click Me" onPress={() => console.log("Button pressed")} />
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
