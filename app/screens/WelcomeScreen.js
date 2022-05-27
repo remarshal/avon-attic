bdcd970d84262cbebbe1e148e81214eed41fd376
remarshal <marshalwaldendev@gmail.com>
2022-05-27 15:37:48 -0400

    added logo

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,49 @@
 import React from "react";
-import { ImageBackground, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
 
-function WelcomeScreen(props) {
+function WelcomeScreen() {
   return (
     <ImageBackground
       style={styles.background}
       source={require("../assets/background.jpg")}
-    ></ImageBackground>
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/avon-attic-logo.png")}
+        />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 50,
+    alignItems: "center",
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#6dceff",
   },
 });
 
